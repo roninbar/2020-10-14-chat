1b3619ea8bfdc07d6b7bfac262fb686cdad3c583
ron <rinbar70@gmail.com>
2020-10-18 00:52:26 +0300

    Build /client-react-redux and update /server/public.

---
+++ server/public/precache-manifest.c7cd3afa205d26d290cc23f8464eb15b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51e5ec43431861f67e7216b011f99146",
+    "url": "/index.html"
+  },
+  {
+    "revision": "949d50be1bf54cf2eef5",
+    "url": "/static/css/main.0e96c88f.chunk.css"
+  },
+  {
+    "revision": "51f0e6b40321a84c492b",
+    "url": "/static/js/2.0a24f9bc.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.0a24f9bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "949d50be1bf54cf2eef5",
+    "url": "/static/js/main.cc7561a5.chunk.js"
+  },
+  {
+    "revision": "07ad8c11d6422fcd5fb0",
+    "url": "/static/js/runtime-main.7732edb6.js"
+  }
+]);
