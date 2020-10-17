84d575bfd2585ef2c99da5ff570fe4152ca7a9b1
ron <rinbar70@gmail.com>
2020-10-18 00:59:43 +0300

    Prepare the server for use with a client-side router.

--- server/app.js
+++ server/app.js
@@ -1,10 +1,8 @@
+/* eslint-disable linebreak-style */
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -14,7 +12,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 module.exports = app;
+
