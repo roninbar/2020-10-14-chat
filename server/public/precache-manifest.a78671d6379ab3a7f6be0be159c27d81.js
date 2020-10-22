266269d903b8ba8182a6de6fd91a41e33332a2d8
ron <rinbar70@gmail.com>
2020-10-23 02:38:08 +0300

    Build /client-react-redux and update /server/public.

---
+++ server/public/precache-manifest.a78671d6379ab3a7f6be0be159c27d81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2430952f6e42551e53f792fcfa58c07e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7ba32a4aa0519118c6f",
+    "url": "/static/css/main.c5da99b4.chunk.css"
+  },
+  {
+    "revision": "c28e70a6e0a40ec19706",
+    "url": "/static/js/2.14bd72c4.chunk.js"
+  },
+  {
+    "revision": "1c538bda74831820fcc36388b7d77cd1",
+    "url": "/static/js/2.14bd72c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7ba32a4aa0519118c6f",
+    "url": "/static/js/main.e9dde01d.chunk.js"
+  },
+  {
+    "revision": "07ad8c11d6422fcd5fb0",
+    "url": "/static/js/runtime-main.7732edb6.js"
+  }
+]);
--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.419b84287b04cb272c811cfab18ecf3d.js"
+  "/precache-manifest.a78671d6379ab3a7f6be0be159c27d81.js"
 );
 
 self.addEventListener('message', (event) => {
